2060678c7c0ba31ffd5d665846c4b767c0a657c8
HansakaJS <hansakajayawarna@gmail.com>
2024-02-02 22:38:18 +0530

    Added the logger messages

--- src/main/java/com/example/BuildREST/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/BuildREST/service/impl/EmployeeServiceImpl.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
+    private static final Logger LOGGER = Logger.getLogger(EmployeeServiceImpl.class.getName());
     private EmployeeRepository employeeRepository;
 
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
@@ -20,26 +23,58 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> findAllEmployees() {
-        return employeeRepository.findAll();
+
+        try{
+            LOGGER.info("Finding all employees");
+            return employeeRepository.findAll();
+        }catch (Exception e){
+            LOGGER.log(Level.SEVERE, "Error occurred while finding all employees", e);
+            throw e;
+        }
     }
 
     @Override
     public Optional<Employee> findEmployee(Integer id) {
-        return employeeRepository.findById(id);
+
+        try {
+            LOGGER.info("Finding the employee");
+            return employeeRepository.findById(id);
+        }catch(Exception e){
+            LOGGER.log(Level.SEVERE, "Error occurred while finding a employee", e);
+            throw e;
+        }
     }
 
     @Override
     public Employee saveEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+        try {
+            LOGGER.info("Saving the employee");
+            return employeeRepository.save(employee);
+        }catch (Exception e){
+            LOGGER.log(Level.SEVERE, "Error occurred while saving the employee", e);
+            throw e;
+        }
     }
 
     @Override
     public Employee updateEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+        try {
+            LOGGER.info("Updating the employee");
+            return employeeRepository.save(employee);
+        }catch(Exception e){
+            LOGGER.log(Level.SEVERE, "Error occurred while updating the employee", e);
+            throw e;
+        }
     }
 
     @Override
     public void deleteEmployee(Integer id) {
-        employeeRepository.deleteById(id);
+        try {
+            LOGGER.info("Deleting the employee");
+            employeeRepository.deleteById(id);
+        }catch(Exception e){
+            LOGGER.log(Level.SEVERE, "Error occurred while deleting the employee", e);
+            throw e;
+        }
     }
 }
